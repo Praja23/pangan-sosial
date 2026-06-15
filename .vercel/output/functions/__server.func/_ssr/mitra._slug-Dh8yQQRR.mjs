e0892c06daa410d77612a8e9f649b1d93dbb34d8
Praja23 <prajaanugerah23@gmail.com>
2026-06-16 02:21:32 +0700

    Update project before deployment

--- .vercel/output/functions/__server.func/_ssr/mitra._slug-Dh8yQQRR.mjs
+++
@@ -1,5 +0,0 @@
-import { j as jsxRuntimeExports } from "../_libs/react.mjs";
-const SplitNotFoundComponent = () => /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "min-h-screen grid place-items-center", children: "Mitra tidak ditemukan" });
-export {
-  SplitNotFoundComponent as notFoundComponent
-};
--- .vercel/output/static/assets/route-DPiYa6ER.js
+++
@@ -1,1 +0,0 @@
-import{j as t,O as o}from"./index-BMqV4kYC.js";const n=()=>t.jsx(o,{});export{n as component};
