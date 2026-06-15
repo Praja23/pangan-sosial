a56173fb663c8eca9f53251834df68bebbce4b3d
Praja23 <prajaanugerah23@gmail.com>
2026-06-16 01:20:25 +0700

    Remove .vercel from repository

--- .vercel/output/functions/__server.func/_ssr/listing._id-seZ8wQes.mjs
+++
@@ -1,5 +0,0 @@
-import { j as jsxRuntimeExports } from "../_libs/react.mjs";
-const SplitNotFoundComponent = () => /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "min-h-screen grid place-items-center", children: "Listing tidak ditemukan" });
-export {
-  SplitNotFoundComponent as notFoundComponent
-};
--- .vercel/output/static/assets/listing._id-CtAe8yWL.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-BMqV4kYC.js";const e=()=>n.jsx("div",{className:"min-h-screen grid place-items-center",children:"Listing tidak ditemukan"});export{e as notFoundComponent};
